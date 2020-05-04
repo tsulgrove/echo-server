b49b0eb3d00cbf65d04dfff1c329e88be9cc1697
Tom <tsulgrove@gmail.com>
2020-05-03 17:18:21 -0700

    complete

--- echo_client.py
+++ echo_client.py
@@ -4,43 +4,53 @@ import traceback
 
 
 def client(msg, log_buffer=sys.stderr):
-    server_address = ('localhost', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
-    # TODO: connect your socket to the server here.
 
-    # you can use this variable to accumulate the entire message received back
-    # from the server
+    server_address = ('127.0.0.1', 10000)
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    
+    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
+
+    #connect to the address
+    sock.connect(server_address)
     received_message = ''
 
-    # this try/finally block exists purely to allow us to close the socket
-    # when we are finished with it
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
-        # TODO: send your message to the server here.
-
-        # TODO: the server should be sending you back your message as a series
-        #       of 16-byte chunks. Accumulate the chunks you get to build the
-        #       entire reply from the server. Make sure that you have received
-        #       the entire message and then you can break the loop.
-        #
-        #       Log each chunk you receive.  Use the print statement below to
-        #       do it. This will help in debugging problems
-        chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+        
+        #send the message, declare buffer, and timeout
+        sock.sendall(msg.encode("utf-8"))
+        buffer_size = 16
+        sock.settimeout(1)
+        
+        while True:
+            
+            #recieve a chunk of the message
+            chunk = sock.recv(buffer_size)
+            
+            #if there is nothing then break
+            if not chunk:
+                break
+            
+            print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+            
+            #add on to recieved message
+            received_message += chunk.decode("utf8")
+            
+    except socket.timeout:
+        #catch the timeout when they are done talking
+        pass       
+    
     except Exception as e:
         traceback.print_exc()
         sys.exit(1)
+        
     finally:
-        # TODO: after you break out of the loop receiving echoed chunks from
-        #       the server you will want to close your client socket.
+        #close up when done
+        sock.close()
         print('closing socket', file=log_buffer)
 
-        # TODO: when all is said and done, you should return the entire reply
-        # you received from the server as the return value of this function.
-
+    return received_message
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
--- echo_server.py
+++ echo_server.py
@@ -6,79 +6,60 @@ import traceback
 def server(log_buffer=sys.stderr):
     # set an address for our server
     address = ('127.0.0.1', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    # TODO: You may find that if you repeatedly run the server script it fails,
-    #       claiming that the port is already used.  You can set an option on
-    #       your socket that will fix this problem. We DID NOT talk about this
-    #       in class. Find the correct option by reading the very end of the
-    #       socket library documentation:
-    #       http://docs.python.org/3/library/socket.html#example
-
+    
+    #set up socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(address)
+    
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
-    # TODO: bind your new sock 'sock' to the address above and begin to listen
-    #       for incoming connections
+    #start listening
+    sock.listen(1)
 
     try:
-        # the outer loop controls the creation of new connection sockets. The
-        # server will handle each incoming connection one at a time.
         while True:
             print('waiting for a connection', file=log_buffer)
-
-            # TODO: make a new socket when a client connects, call it 'conn',
-            #       at the same time you should be able to get the address of
-            #       the client so we can report it below.  Replace the
-            #       following line with your code. It is only here to prevent
-            #       syntax errors
-            conn, addr = ('foo', ('bar', 'baz'))
+            
+            #accept connection, set buffer, and timeout
+            conn, addr = sock.accept()
+            buffer_size = 16
+            conn.settimeout(5)
+    
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
 
-                # the inner loop will receive messages sent by the client in
-                # buffers.  When a complete message has been received, the
-                # loop will exit
                 while True:
-                    # TODO: receive 16 bytes of data from the client. Store
-                    #       the data you receive as 'data'.  Replace the
-                    #       following line with your code.  It's only here as
-                    #       a placeholder to prevent an error in string
-                    #       formatting
-                    data = b''
+                    #recive message from client
+                    received_message = conn.recv(buffer_size)
+                    
+                    data = received_message
                     print('received "{0}"'.format(data.decode('utf8')))
                     
-                    # TODO: Send the data you received back to the client, log
-                    # the fact using the print statement here.  It will help in
-                    # debugging problems.
+                    #send message back
+                    conn.sendall(data)
+                    
                     print('sent "{0}"'.format(data.decode('utf8')))
                     
-                    # TODO: Check here to see whether you have received the end
-                    # of the message. If you have, then break from the `while True`
-                    # loop.
-                    # 
-                    # Figuring out whether or not you have received the end of the
-                    # message is a trick we learned in the lesson: if you don't
-                    # remember then ask your classmates or instructor for a clue.
-                    # :)
+                    #if there is no message then stop this madness in the name of the king
+                    if not data:
+                        break
+                    
             except Exception as e:
                 traceback.print_exc()
                 sys.exit(1)
+                
             finally:
-                # TODO: When the inner loop exits, this 'finally' clause will
-                #       be hit. Use that opportunity to close the socket you
-                #       created above when a client connected.
+                #close up when done.
+                conn.close()
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
 
     except KeyboardInterrupt:
-        # TODO: Use the python KeyboardInterrupt exception as a signal to
-        #       close the server socket and exit from the server function.
-        #       Replace the call to `pass` below, which is only there to
-        #       prevent syntax problems
-        pass
+        #kill command
+        sock.close()
         print('quitting echo server', file=log_buffer)
 
 
